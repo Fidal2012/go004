8bf0e56bf7fce0e7238784b6d27800fc2d6a94f3
dongzeXD <65775622@qq.com>
2021-12-13 11:40:27 +0800

    fix slice bug

--- week02/main.go
+++ week02/main.go
@@ -12,42 +12,42 @@ import (
 // 2. 类似Get/Set操作，在业务中多是需要返回“未找到ID为XXX的用户”，需要wrap关键查询字段并返回
 
 type User struct {
-	id int
+	id   int
 	name string
 }
 
-func Get(id int)(User, error){
+func Get(id int) (User, error) {
 	err := sql.ErrNoRows
 	err = errors.Wrap(err, "Get User where id = "+strconv.Itoa(id))
 	return User{}, err
 }
 
-func Select(id ...int)([]User, error){
-	var err,sqlErr error
-	result := make([]User, len(id))
-	for i := range id{
+func Select(id ...int) ([]User, error) {
+	var err, sqlErr error
+	result := make([]User, 0, len(id))
+	for i := range id {
 		// operation
-		u := User{id:i}
-		if i/2 == 0{
+		u := User{id: i}
+		if i/2 == 0 {
 			sqlErr = sql.ErrNoRows
-		}else{
+		} else {
 			sqlErr = nil
 		}
 		time.Sleep(time.Second)
 		// operation over, return error
 
 		// handle error
-		if sqlErr != nil{
-			if sqlErr == sql.ErrNoRows{
+		if sqlErr != nil {
+			if sqlErr == sql.ErrNoRows {
 				// print log
 				continue
 			}
 			// return first error
-			if err == nil{
+			if err == nil {
 				err = errors.Wrap(sqlErr, "select User where id = "+strconv.Itoa(i))
 			}
 		}
-		result = append(result,u)
+		result = append(result, u)
 	}
 	return result, err
 }
